6e9a9d1b0cb450696339100b75778aa94bf919c1
lukaskolodz <60406785+lukaskolodz@users.noreply.github.com>
2020-02-10 12:32:51 +0100

    js
    dodanie bibliotek  jqwery

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    
+    var mymap = L.map('mapa',{center:[52.1,21.0],zoom:10});
+var lyrOSM=L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png'); 
+mymap.addLayer(lyrOSM);
+
+    
+})
