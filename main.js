5d9e8263188f2a40343eab3ceea4159d0c00d516
lukaskolodz <60406785+lukaskolodz@users.noreply.github.com>
2020-02-13 00:12:07 +0100

    próba naprawy js tak by mozna było zobaczyć na stronie osm
    przesledziłem ten toutorial na leaflecie ale teoretyczine wszystko jest dobrze, więc przeszedłem do skryptu który mi Pan wysłałi zauważyłem że nie ma pewnej funkcji więc dodałem ją znajduje się becnie w 18 wersie, w celu jej podlączenie pobrałem z sieci bibliotekę leafleta w nadzieji że może to rozwiązać problem ale niestety tak się nie stało

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-$(document).ready(function(){
-    
+
+    $(document).ready(function(){
     var mymap = L.map('mapa',{center:[52.1,21.0],zoom:10});
 var lyrOSM=L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png'); 
 mymap.addLayer(lyrOSM);
-
+});
     
-})
