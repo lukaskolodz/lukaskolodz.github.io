a02715387a11bae39353c52dc488e0483b94706e
lukaskolodz <60406785+lukaskolodz@users.noreply.github.com>
2020-02-20 22:17:57 +0100

    zmiany w markerze i popupie
    zmieniłem marker na marker typu .png usunąłem formularz  z popupa i próbowałem umieścić go gdzies indziej np w alercie ale bez skutku, myślę że muszę przerobić kurs html o formularz i wtedy wyjdzie mi to poprawnie, co do 'katalogowania' klicków to obejrzałem odc zelenta z serii js o wisielcu i mam pytanie czy umieszczać klicki w tablicy tak jak on to robił tylko że nie ręcznie a np przy pomocy jakiejś pętli?

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-
 $(document).ready(function(){
     var mymap = L.map('mapa',{center:[52.1,21.0],zoom:10});
-    var lyrOSM=L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png'); 
+    var lyrOSM=L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
     mymap.addLayer(lyrOSM);
     
     /*tworzenie zmiennej w formie własnej ikonki*/
     var myicon = L.icon({
-        iconUrl: 'https://as1.ftcdn.net/jpg/02/80/87/48/500_F_280874872_tVNxGcKAMB3s1rVuNtcB8MeloTb0PV28.jpg',
-        iconSize: [50,50],
-        iconAnchor: [25,25],
-        popupAnchor: [0,0],
+        iconUrl: 'iconb.png',
+        iconSize: [20,34],
+        iconAnchor: [10,34],
+        popupAnchor: [0,-34],
     })
     
   var formularz = '<select id="cars" name="cars"><option value="volvo">Volvo</option><option value="saab">Saab</option><option value="fiat">Fiat</option> ' 
@@ -20,7 +19,7 @@ $(document).ready(function(){
        var marker = L
        .marker(event.latlng,{icon: myicon})
        .addTo(mymap)
-       .bindPopup(formularz+
+       .bindPopup(
            'Współrzędne geograficzne:<br>'
                   + event.latlng.lng.toFixed(3)
                   +' E'
@@ -28,8 +27,6 @@ $(document).ready(function(){
                   +event.latlng.lat.toFixed(3)
                   +' N')
        .openPopup();
-    
-        
        
     })
    
